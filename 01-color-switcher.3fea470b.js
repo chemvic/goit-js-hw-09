e2dfee1c2d01b0c0cbe55fac76fc5cd60899c74c
chemvic <112618402+chemvic@users.noreply.github.com>
2023-02-22 11:56:54 +0000

    Deploying to gh-pages from @ chemvic/goit-js-hw-09@5ef1a61ac11bbce88a71fe7ac56f483ce7d780af 🚀

---
+++ 01-color-switcher.3fea470b.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]");let n=null;t.addEventListener("click",(function(e){t.setAttribute("disabled",!0),n=setInterval((()=>{document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3)})),e.addEventListener("click",(function(e){clearInterval(n),t.removeAttribute("disabled")}));
+//# sourceMappingURL=01-color-switcher.3fea470b.js.map
---
+++ 01-color-switcher.d265f3d7.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),n=null;t.addEventListener("click",(function(e){t.setAttribute("disabled",!0),n=setInterval((function(){document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3)})),e.addEventListener("click",(function(e){clearInterval(n),t.removeAttribute("disabled")}))}();
+//# sourceMappingURL=01-color-switcher.d265f3d7.js.map
