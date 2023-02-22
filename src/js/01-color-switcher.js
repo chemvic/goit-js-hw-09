5ef1a61ac11bbce88a71fe7ac56f483ce7d780af
chemvic <chemvic79@gmail.com>
2023-02-22 13:56:30 +0200

    первая задача

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,24 @@
+const buttonStartEl=document.querySelector("button[data-start]");
+const buttonStopEl=document.querySelector("button[data-stop]");
+let intervalId = null;
 
+buttonStartEl.addEventListener('click', onChangeColor);
+
+function onChangeColor(event) {
+    buttonStartEl.setAttribute("disabled", true);  
+  
+
+    intervalId = setInterval(() => {
+         document.body.style.backgroundColor = getRandomHexColor();
+    }, 1000); 
+}
+
+buttonStopEl.addEventListener('click', onStop);
+function onStop (event){
+    clearInterval(intervalId);
+    buttonStartEl.removeAttribute("disabled");
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
