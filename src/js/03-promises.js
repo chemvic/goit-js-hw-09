044090228b2b26d062dd38b219b5d712cc4b4028
chemvic <chemvic79@gmail.com>
2023-02-26 17:59:39 +0200

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,16 +1,15 @@
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 const formEl = document.querySelector('.form');
 
-
 formEl.addEventListener('submit', onSubmit);
 
 
 
 function onSubmit(event) {
   event.preventDefault();
-  let delay = 0;
+ 
   const formElemenents = event.currentTarget.elements;
-  delay = Number(formElemenents.delay.value);
+  let delay = Number(formElemenents.delay.value);
   let step = Number(formElemenents.step.value);
   let amount = Number(formElemenents.amount.value);
 
@@ -20,7 +19,8 @@ for (let i = 1; i <= amount; i+=1) {
     .then(({ position, delay }) => { Notify.success(`Fulfilled promise ${position} in ${delay}ms`); })
     .catch(({position, delae})=>{Notify.failure(`Rejected promise ${position} in ${delay}ms`);});
   delay += step;
-}
+  }
+  
 
 }
 
